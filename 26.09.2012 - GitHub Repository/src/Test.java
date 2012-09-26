88db75a99e6861d7e0e4265af93fabc402a79463
D.Wiezorek <dominic.wiezorek@gmx.de>
2012-09-26 08:46:24 +0200

    Initialisierung des Projekts...

---
+++ 26.09.2012 - GitHub Repository/src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author e0_wiezorek
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
